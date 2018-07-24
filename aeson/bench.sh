064e3322332ebedf1a25aaa776effa985244c9e1
klebinger.andreas@gmx.at <klebinger.andreas@gmx.at>
2018-07-24 12:54:16 +0200

    Fix flag quoting, ignore result dir

--- aeson/bench.sh
+++ aeson/bench.sh
@@ -58,9 +58,9 @@ BENCH_PREFIX=${DIR_NAME#aeson_}
 
 for i in {0..3};
 do
-    HC_FLAGS=FLAG_STRS[$i]
-    echo "Configure for $FLAG_NAMES[$i] - $HC_FLAGS"
-    cabal new-configure all -w "$HC" --allow-newer=base,primitive --ghc-options="${HC_FLAGS}" --bindir="$BIN_DIR" --enable-benchmarks --store-dir="./store"
+    HC_FLAGS=$FLAG_STRS[$i]
+    echo "Configure for $FLAG_NAMES[$i] - ${HC_FLAGS}"
+    cabal new-configure all -w "$HC" --allow-newer=base,primitive --ghc-options="${HC_FLAGS}" --enable-benchmarks --store-dir="./store"
     cabal new-build all -j4
 
     for benchmark in aeson-benchmark-typed aeson-benchmark-micro aeson-benchmark-map aeson-benchmark-json-parse aeson-benchmark-foldable aeson-benchmark-escape aeson-benchmark-dates aeson-benchmark-compare-with-json aeson-benchmark-compare aeson-benchmark-auto-compare aeson-benchmark-aeson-parse aeson-benchmark-aeson-encode;
