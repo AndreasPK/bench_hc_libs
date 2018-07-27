ee7c8cb13abccebabe9a69a066078c2a8c454c94
klebinger.andreas@gmx.at <klebinger.andreas@gmx.at>
2018-07-27 14:29:04 +0200

    Remove old cruft

--- mparsec/bench.sh
+++ mparsec/bench.sh
@@ -8,7 +8,6 @@ mkdir -p "$LOG_DIR"
 
 if [ -z ${1} ]; then
     echo "Please specify a compiler: $0 <HC>"
-    #exit
     HC="C:\\ghc\\msys64\\home\\Andi\\ghc_layout\\inplace\\bin\\ghc-stage2.exe"
 else
     HC="$1"
@@ -17,10 +16,7 @@ fi
 if [ ! -d "megaparsec" ]; then
   git clone https://github.com/mrkkrp/megaparsec.git
   sed -i "1i{-# LANGUAGE UndecidableInstances  #-}" megaparsec/Text/Megaparsec/Error.hs
-  #sed "s/name: containers/name: containers-bench/" containers/containers.cabal  -i
 fi
-#cd aeson
-#cabal new-update
 
 if [ ! -d "primitive" ]; then
   git clone https://github.com/haskell/primitive.git
@@ -42,8 +38,6 @@ cabal new-update
 DIR_NAME=${PWD##*/}
 COMPILER_NAME=${DIR_NAME#c_}
 BENCHMARKS="bench-speed"
-# STORE_DIR=~/.store_${COMPILER_NAME}
-# STORE="--store-dir=${STORE_DIR} "
 for i in {0..3};
 do
     HC_FLAGS="${FLAG_STRS[$i]}"
