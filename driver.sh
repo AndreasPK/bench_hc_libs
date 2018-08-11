e2ed8e26329e7836f77eaa099fdd5c8d12fe9640
klebinger.andreas@gmx.at <klebinger.andreas@gmx.at>
2018-08-12 01:17:30 +0200

    Add a driver for the drivers  ..

---
+++ driver.sh
@@ -0,0 +1,26 @@
+cd vector
+bash driver.sh
+cd ..
+
+cd mparsec
+bash driver.sh
+cd ..
+
+cd aeson
+bash driver.sh
+cd ..
+
+cd containers
+bash driver.sh
+cd ..
+
+cd perf-xml
+bash driver.sh
+cd ..
+
+cd text
+bash driver.sh
+cd ..
+
+
+
