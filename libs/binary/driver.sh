2f5820e82bac59822a02887dae41d9a892b13114
klebinger.andreas@gmx.at <klebinger.andreas@gmx.at>
2019-04-13 16:56:20 +0200

    Update binary driver

--- libs/binary/driver.sh
+++ libs/binary/driver.sh
@@ -1,80 +1,60 @@
+#!/bin/bash
+
+#Execute using ./driver <pathToGhc> [aliasForGhc [BuildFlags [FlagAlias]]]
+#If any optional flag is given all flags before that must also be given.
+
 set -x
 
-for compiler in allCalls noCalls;
-#for compiler in adjusted  allCalls  head  noCalls  someCalls  vanilla
+LOG_DIR=../benchResults
+mkdir -p "$LOG_DIR"
+
+BENCHMARKS="get builder generics-bench put"
+
+HC=${1:-ghc}
+HC_NAME=${2:-vanilla}
+HC_FLAGS=${3:-""}
+FLAG_ALIAS=${4:-$HC_NAME}
+# HC_NAME=${1:-vanilla}
+
+#We use a compiler and a build name in case we want to benchmark different flags.
+#args: compiler-exec compiler-name build-flags log-name bench-name
+function runBenchmark() {
+    HC="$1"
+    HC_NAME="$2"
+    HC_FLAGS="$3"
+    NAME="$4"
+    BENCHMARK="$5"
+    STORE_DIR="s_$HC_NAME"
+    BUILD_DIR="b_$NAME"
+
+    echo "Benchmark: $NAME"
+    cabal --store-dir="$HOME/.${STORE_DIR}" new-run --builddir="$BUILD_DIR" -w "$HC" --ghc-options="${HC_FLAGS}" --enable-benchmarks --disable-tests \
+        "$BENCHMARK" -- --csv "../$LOG_DIR/${HC_NAME}.${NAME}.${BENCHMARK}.csv" -n1
+}
+
+
+# custom per library, run inside of library repository
+function setup_benchmarks() {
+    mkdir "build" -p
+    cp cabal.project "build"
+    cd "build"
+
+    #2019-04-13 Monad fail not patched yet so use fork
+    if [ ! -d "binary" ]; then
+        git clone https://github.com/kolmodin/binary
+    fi
+    cp binary/generics-bench.cache.gz .
+    sed "s/name:            binary/name: binary-bench/" binary/binary.cabal  -i
+    cabal new-update
+}
+
+setup_benchmarks
+
+# "/e/ghc_regSpill/inplace/bin/ghc-stage2.exe"
+for BENCHMARK in $BENCHMARKS
 do
-    unameOut="$(uname -s)"
-    case $unameOut in
-    MINGW*)
-        HC="C:\\ghc\\msys64\\home\\Andi\\trees\\${compiler}\\inplace\\bin\\ghc-stage2.exe";
-        HC_HEAD="C:\\ghc\\msys64\\home\\Andi\\trees\\head\\inplace\\bin\\ghc-stage2.exe";
-        ;;
-    *)
-        HC=~/trees/${compiler}/inplace/bin/ghc-stage2 ;
-        HC_HEAD=~/trees/head/inplace/bin/ghc-stage2 ;;
-    esac
-
-    mkdir "c_${compiler}" -p
-    cp cabal.project "c_${compiler}"
-    cd "c_${compiler}"
-
-
-        LOG_DIR=../benchResults
-        FLAG_NAMES=('vanilla' 'all' 'some' 'none' 'adjusted')
-        FLAG_STRS=('-fno-new-blocklayout -fvanilla-blocklayout' '-fnew-blocklayout -fcfg-weights=callWeight=310' '-fnew-blocklayout -fcfg-weights=callWeight=300' '-fnew-blocklayout -fcfg-weights=callWeight=-900' '-fno-new-blocklayout -fno-vanilla-blocklayout')
-        NFLAGS=$((${#FLAG_NAMES[@]} - 1))
-        mkdir -p "$LOG_DIR"
-
-        if [ ! -d "binary" ]; then
-            git clone https://github.com/kolmodin/binary.git
-            cd binary
-                git checkout 38adf7ce1ad6a497fba61de500c3f35b186303a9
-                git reset --hard
-            cd ..
-            sed "s/name:            binary/name: binary-bench/" binary/binary.cabal  -i
-        fi
-
-        if [ ! -f "generics-bench.cache.gz" ]; then
-            cp binary/generics-bench.cache.gz .
-        fi
-
-        cabal new-update
-
-        #Build with different flags
-
-        DIR_NAME=${PWD##*/}
-        COMPILER_NAME=${DIR_NAME#c_}
-        BENCHMARKS=('get' 'builder' 'generics-bench' 'put')
-        for i in $(seq 0 $NFLAGS);
-        do
-            HC_FLAGS="${FLAG_STRS[$i]}"
-            FLAG_VARIANT="${FLAG_NAMES[$i]}"
-            STORE_DIR=~/.store_"${compiler}_${FLAG_VARIANT}"
-            BUILD_DIR=d-"${compiler}_$FLAG_VARIANT"
-            echo "Configuration ${FLAG_NAMES[$i]} - ${HC_FLAGS}"
-            cabal --store-dir="$STORE_DIR" new-build --builddir="$BUILD_DIR" -w "$HC" --ghc-options="${HC_FLAGS}" --enable-benchmarks --disable-tests "${BENCHMARKS[@]}"
-
-            for benchmark in "${BENCHMARKS[@]}";
-            do
-                echo "Benchmark: $benchmark"
-                cabal --store-dir="$STORE_DIR" new-run --builddir="$BUILD_DIR" -w "$HC" --ghc-options="${HC_FLAGS}" --enable-benchmarks --disable-tests \
-                    "$benchmark" -- --csv "$LOG_DIR/${COMPILER_NAME}.${FLAG_NAMES[$i]}.${benchmark}.csv" -L10
-            done
-        done
-
-        HC_FLAGS=""
-        FLAG_VARIANT="head"
-        STORE_DIR=~/.store_"${FLAG_VARIANT}"
-        BUILD_DIR=d-"$FLAG_VARIANT"
-        HC="$HC_HEAD"
-        cabal --store-dir="$STORE_DIR" new-build --builddir="$BUILD_DIR" -w "$HC" --ghc-options="${HC_FLAGS}" --enable-benchmarks --disable-tests "${BENCHMARKS[@]}"
-        for benchmark in "${BENCHMARKS[@]}";
-        do
-            echo "Benchmark: $benchmark"
-            cabal --store-dir="$STORE_DIR" new-run --builddir="$BUILD_DIR" -w "$HC" --ghc-options="${HC_FLAGS}" --enable-benchmarks --disable-tests \
-               "$benchmark" -- --csv "$LOG_DIR/${COMPILER_NAME}.${FLAG_VARIANT}.${benchmark}.csv" -L10
-        done
-        wait
-
-    cd ..
+runBenchmark "${HC}" "${HC_NAME}" "${HC_FLAGS}" "${HC_NAME}" "${BENCHMARK}";
 done
+
+cd ..
+
