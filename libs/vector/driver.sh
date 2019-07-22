73fd5088417193f3b4c5ff3c315e58c6d93bd3dc
Andreas Klebinger <klebinger.andreas@gmx.at>
2019-07-22 17:31:10 +0200

    Update vector benchmarks

--- libs/vector/driver.sh
+++ libs/vector/driver.sh
@@ -1,89 +1,56 @@
 set -x
 
-for compiler in allCalls noCalls;
-#for compiler in adjusted  allCalls  head  noCalls  someCalls  vanilla
+
+LOG_DIR=benchResults
+mkdir -p "$LOG_DIR"
+LOG_DIR="../$LOG_DIR"
+
+DEF_BENCHMARKS="algorithms"
+
+HC=${1:-ghc}
+HC_NAME=${2:-vanilla}
+HC_FLAGS=${3:-""}
+FLAG_ALIAS=${4:-$HC_NAME}
+BENCHMARKS=${5:-$DEF_BENCHMARKS}
+
+#We use a compiler and a build name in case we want to benchmark different flags.
+#args: compiler-exec compiler-name build-flags log-name bench-name
+function runBenchmark() {
+    HC="$1"
+    HC_NAME="$2"
+    HC_FLAGS="$3"
+    NAME="$4"
+    BENCHMARK="$5"
+    STORE_DIR="s_$HC_NAME"
+    BUILD_DIR="b_$NAME"
+
+    echo "Benchmark: $NAME"
+    #Use -L10 since these are pretty noisy
+    cabal --store-dir="$HOME/.${STORE_DIR}" new-run --builddir="$BUILD_DIR" -w "$HC" --ghc-options="${HC_FLAGS}" --enable-benchmarks --disable-tests \
+        "$BENCHMARK" -- --csv "$LOG_DIR/${HC_NAME}.${NAME}.${BENCHMARK}.csv" -L10
+}
+
+
+# custom per library, run inside of library repository
+function setup_benchmarks() {
+    mkdir "build" -p
+    cp cabal.project "build"
+    cd "build"
+
+    if [ ! -d "vector" ]; then
+        git clone https://github.com/haskell/vector.git
+
+    fi
+    cabal new-update
+}
+
+setup_benchmarks
+
+# "/e/ghc_regSpill/inplace/bin/ghc-stage2.exe"
+for BENCHMARK in $BENCHMARKS
 do
-    unameOut="$(uname -s)"
-    case $unameOut in
-    MINGW*)
-        HC="C:\\ghc\\msys64\\home\\Andi\\trees\\${compiler}\\inplace\\bin\\ghc-stage2.exe" ;;
-    *)
-        HC=~/trees/${compiler}/inplace/bin/ghc-stage2 ;;
-    esac
-    HC_HEAD="$HOME/trees/head/inplace/bin/ghc-stage2"
-
-    mkdir "c_${compiler}" -p
-    cp bench.sh "c_${compiler}"
-    cp cabal.project "c_${compiler}"
-    cd "c_${compiler}"
-
-
-        LOG_DIR=../benchResults
-        mkdir -p "$LOG_DIR"
-        FLAG_NAMES=('vanilla' 'all' 'some' 'none' 'adjusted')
-        FLAG_STRS=( '-fno-new-blocklayout -fvanilla-blocklayout'
-                    '-fnew-blocklayout -fcfg-weights=callWeight=310'
-                    '-fnew-blocklayout -fcfg-weights=callWeight=300'
-                    '-fnew-blocklayout -fcfg-weights=callWeight=-900'
-                    '-fno-new-blocklayout -fno-vanilla-blocklayout')
-        NFLAGS=$((${#FLAG_NAMES[@]} - 1))
-
-
-        if [ ! -d "vector" ]; then
-        git clone https://github.com/AndreasPK/vector.git
-        #sed -i "1i{-# LANGUAGE UndecidableInstances  #-}" megaparsec/Text/Megaparsec/Error.hs
-        fi
-
-        if [ ! -d "primitive" ]; then
-        git clone https://github.com/haskell/primitive.git
-        cd primitive
-        git reset --hard a2af610
-        cd ..
-        fi
-
-        if [ ! -d "vector-algorithms" ]; then
-        curl https://hub.darcs.net/dolio/vector-algorithms/dist --output vector-algorithms.zip
-        unzip vector-algorithms.zip
-        sed "s/Odph/O2/" -i vector-algorithms/vector-algorithms.cabal
-        fi
-
-        cabal new-update
-
-        #Build with different flags
-        #-ddump-asm -ddump-opt-cmm -ddump-simpl -ddump-cfg-weights -ddump-to-file -dsuppress-all
-
-        DIR_NAME=${PWD##*/}
-        BENCHMARKS="algorithms"
-        COMPILER_NAME=${DIR_NAME#c_}
-        for i in $(seq 0 $NFLAGS);
-        do
-            HC_FLAGS="${FLAG_STRS[$i]}"
-            FLAG_VARIANT="${FLAG_NAMES[$i]}"
-            STORE_DIR=~/.store_"${FLAG_VARIANT}"
-            BUILD_DIR=d-"${FLAG_VARIANT}"
-            echo "Configuration ${FLAG_NAMES[$i]} - ${HC_FLAGS}"
-            cabal --store-dir="$STORE_DIR" new-build --builddir="$BUILD_DIR" -w "$HC" --ghc-options="${HC_FLAGS}" --enable-benchmarks --disable-tests -j5 all
-
-            for benchmark in ${BENCHMARKS};
-            do
-                echo "Benchmark: $benchmark"
-                cabal --store-dir="$STORE_DIR" new-run --builddir="$BUILD_DIR" -w "$HC" --ghc-options="${HC_FLAGS}" --enable-benchmarks --disable-tests \
-                    "exe:$benchmark" -- --seed 1230891 --csv "$LOG_DIR/${COMPILER_NAME}.${FLAG_VARIANT}.${benchmark}.csv" -L10
-            done
-        done
-
-        HC_FLAGS=""
-        FLAG_VARIANT="head"
-        STORE_DIR=~/.store_"${FLAG_VARIANT}"
-        BUILD_DIR=d-"$FLAG_VARIANT"
-        HC="$HC_HEAD"
-        cabal --store-dir="$STORE_DIR" new-build --builddir="$BUILD_DIR" -w "$HC" --ghc-options="${HC_FLAGS}" --enable-benchmarks --disable-tests -j5 all
-        for benchmark in ${BENCHMARKS};
-        do
-            echo "Benchmark: $benchmark"
-            cabal --store-dir="$STORE_DIR" new-run --builddir="$BUILD_DIR" -w "$HC" --ghc-options="${HC_FLAGS}" --enable-benchmarks --disable-tests \
-                "exe:$benchmark" -- --seed 1230891 --csv "$LOG_DIR/${COMPILER_NAME}.${FLAG_VARIANT}.${benchmark}.csv" -L10
-        done
-
-    cd ..
+    runBenchmark "${HC}" "${HC_NAME}" "${HC_FLAGS}" "${HC_NAME}" "${BENCHMARK}";
 done
+
+cd ..
+
