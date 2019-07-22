787907723a8710f6cfc025adf8f3356262ea49bc
Andreas Klebinger <klebinger.andreas@gmx.at>
2019-07-22 12:47:44 +0200

    New hackage head, allow passing benchmarks

--- libs/containers/driver.sh
+++ libs/containers/driver.sh
@@ -10,7 +10,7 @@ LOG_DIR=benchResults
 mkdir -p "$LOG_DIR"
 LOG_DIR="../$LOG_DIR"
 
-BENCHMARKS="set-operations-set set-operations-map set-operations-intset set-operations-intmap
+DEF_BENCHMARKS="set-operations-set set-operations-map set-operations-intset set-operations-intmap
             set-benchmarks sequence-benchmarks map-benchmarks lookupge-map lookupge-intmap
             intset-benchmarks intmap-benchmarks"
 
@@ -18,7 +18,7 @@ HC=${1:-ghc}
 HC_NAME=${2:-vanilla}
 HC_FLAGS=${3:-""}
 FLAG_ALIAS=${4:-$HC_NAME}
-# HC_NAME=${1:-vanilla}
+BENCHMARKS=${5:-$DEF_BENCHMARKS}
 
 #We use a compiler and a build name in case we want to benchmark different flags.
 #args: compiler-exec compiler-name build-flags log-name bench-name
@@ -61,7 +61,7 @@ setup_benchmarks
 # "/e/ghc_regSpill/inplace/bin/ghc-stage2.exe"
 for BENCHMARK in $BENCHMARKS
 do
-runBenchmark "${HC}" "${HC_NAME}" "${HC_FLAGS}" "${HC_NAME}" "${BENCHMARK}";
+    runBenchmark "${HC}" "${HC_NAME}" "${HC_FLAGS}" "${HC_NAME}" "${BENCHMARK}";
 done
 
 cd ..
